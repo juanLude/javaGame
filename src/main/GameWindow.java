17a1ad47add7b67d4bc92c3b22a9b0b22c6d3bf8
Juan Ludevid <jludevid@myune.edu.au>
2022-10-16 15:27:50 +1100

    first commit

---
+++ src/main/GameWindow.java
@@ -0,0 +1,13 @@
+package main;
+
+import javax.swing.*;
+
+public class GameWindow {
+    private JFrame jFrame;
+
+    public GameWindow(){
+        jFrame = new JFrame();
+        jFrame.setSize(400,400);
+        jFrame.setVisible(true);
+    }
+}
